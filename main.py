ed6cdd41709d2f795ecd23c64b6ebfe3991ee707
Juan <juan_salazar82182@elpoli.edu.co>
2025-06-29 04:16:53 -0500

    entregable sin probar

---
+++ main.py
@@ -0,0 +1,30 @@
+from pandas import DataFrame
+from joblib import load
+from pydantic import BaseModel, ValidationError
+from fastapi import FastAPI, HTTPException
+
+model = load("pipeline_reg.joblib")
+
+app = FastAPI()
+
+class DataPredict(BaseModel):
+    data_to_predict: list[list] = [[17, 1, 0, 2, 19.833723, 7, 1, 2, 0, 0, 1, 0, 2.929196], 
+                                   [18, 0, 0, 1, 15.408756, 0, 0, 1, 0, 0, 0, 0, 3.042915]]
+
+@app.post("/predict")
+def predict(request: DataPredict):
+    try:
+        list_data = request.data_to_predict
+        df_data = DataFrame(list_data, columns=['Age', 'Gender', 'Ethnicity', 'ParentalEducation', 'StudyTimeWeekly', 
+                                                'Absences', 'Tutoring', 'ParentalSupport', 'Extracurricular', 'Sports', 'Music', 'Volunteering', 'GPA'])
+
+        prediction = model.predict(df_data)
+        return {"prediction": prediction.tolist()}
+    except ValidationError as ve:
+        raise HTTPException(status_code=400, detail=ve.errors())
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/")
+def home():
+    return {'Universidad EIA': 'MLOps'}
