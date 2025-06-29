c5cc007c930c0126119781e4686ebbfc7801a2fa
Juan Salazar <juan_salazar82182@elpoli.edu.co>
2025-06-29 13:02:39 -0500

    Funcionando en local

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ model = load("pipeline_reg.joblib")
 app = FastAPI()
 
 class DataPredict(BaseModel):
-    data_to_predict: list[list] = [[17, 1, 0, 2, 19.833723, 7, 1, 2, 0, 0, 1, 0, 2.929196], 
-                                   [18, 0, 0, 1, 15.408756, 0, 0, 1, 0, 0, 0, 0, 3.042915]]
+    data_to_predict: list[list] = [[17, 1, 0, 2, 19.833723, 7, 1, 2, 0, 0, 1, 0], 
+                                   [18, 0, 0, 1, 15.408756, 0, 0, 1, 0, 0, 0, 0]]
 
 @app.post("/predict")
 def predict(request: DataPredict):
     try:
         list_data = request.data_to_predict
-        df_data = DataFrame(list_data, columns=['Age', 'Gender', 'Ethnicity', 'ParentalEducation', 'StudyTimeWeekly', 
-                                                'Absences', 'Tutoring', 'ParentalSupport', 'Extracurricular', 'Sports', 'Music', 'Volunteering', 'GPA'])
-
+        df_data = DataFrame(list_data, columns=['Age', 'Gender', 'Ethnicity', 'ParentalEducation', 'StudyTimeWeekly', 'Absences', 'Tutoring', 'ParentalSupport', 'Extracurricular', 'Sports', 'Music', 'Volunteering'])   
         prediction = model.predict(df_data)
         return {"prediction": prediction.tolist()}
+
     except ValidationError as ve:
         raise HTTPException(status_code=400, detail=ve.errors())
     except Exception as e:
